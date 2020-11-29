c2ed3ca70238b88c0475d5b29bd427dd8ba489f7
Ellysk <ekoka56@gmail.com>
2020-11-29 22:03:29 +0000

    Add font schemes for the project. Add Extension file for all the class extensions fields and methods.

---
+++ Cravy-Partner/Extensions.swift
@@ -0,0 +1,94 @@
+//
+//  Extensions.swift
+//  Cravy-Partner
+//
+//  Created by Cravy on 29/11/2020.
+//  Copyright © 2020 Cravy. All rights reserved.
+//
+
+import UIKit
+
+//MARK: - UI Extensions
+
+
+
+//MARK: - UIFont
+extension UIFont {
+    /// Returns an AvenirNext-Medium font style
+    static var medium: UIFontDescriptor {
+        return addAtrributeName(name: "AvenirNext-Medium")
+    }
+    
+    /// Returns an AvenirNext-Bold font style
+    static var bold: UIFontDescriptor {
+        return addAtrributeName(name: "AvenirNext-Bold")
+    }
+    
+    /// Returns an AvenirNext-DemiBold font style
+    static var demiBold: UIFontDescriptor {
+        return addAtrributeName(name: "AvenirNext-DemiBold")
+    }
+    
+    /// Returns an AvenirNext-Regular font style
+    static var regular: UIFontDescriptor {
+        return addAtrributeName(name: "AvenirNext-Regular")
+    }
+    
+    /// Returns an AvenirNext-MediumItalic font style
+    static var mediumItalic: UIFontDescriptor {
+        return addAtrributeName(name: "AvenirNext-MediumItalic")
+    }
+    
+    /// Returns an AvenirNext-Heavy font style
+    static var heavy: UIFontDescriptor {
+        return addAtrributeName(name: "AvenirNext-Heavy")
+    }
+    
+    private static func addAtrributeName(name: String) -> UIFontDescriptor{
+        return UIFontDescriptor(fontAttributes: [UIFontDescriptor.AttributeName.name : name])
+    }
+}
+
+//MARK: - UIFontDescriptor
+extension UIFontDescriptor {
+    /// Returns a font with a size of 32
+    var large: UIFont {
+        return UIFont(descriptor: self, size: 32)
+    }
+    
+    /// Returns a font with a size of 24
+    var medium: UIFont {
+        return UIFont(descriptor: self, size: 24)
+    }
+    
+    /// Returns a font with a size of 16
+    var small: UIFont {
+        return UIFont(descriptor: self, size: 16)
+    }
+    
+    /// Returns a font with a size of 12
+    var xSmall: UIFont {
+        return UIFont(descriptor: self, size: 12)
+    }
+}
+
+//MARK: - UILabel
+extension UILabel {
+    /// Underline the text displayed by the label. This method is nil safe.
+    func underline() {
+        if let textString = self.text {
+            let attributedString = NSMutableAttributedString(string: textString)
+            attributedString.addAttribute(NSAttributedString.Key.underlineStyle,
+                                          value: NSUnderlineStyle.single.rawValue,
+                                          range: NSRange(location: 0, length: attributedString.length))
+            attributedText = attributedString
+        }
+    }
+}
+
+
+//MARK: - Foundation Extensions
+
+
+
+
