35dbbd91e3003a2c768dde25f3274acd8ff72da8
Ellysk <ekoka56@gmail.com>
2020-11-29 19:40:18 +0000

    Initial Commit

---
+++ Cravy-Partner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cravy-Partner
+//
+//  Created by Cravy on 29/11/2020.
+//  Copyright © 2020 Cravy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
