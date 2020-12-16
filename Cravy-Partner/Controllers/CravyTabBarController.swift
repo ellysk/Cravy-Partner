80e1de8267f4a13a60593b21e5c96162fd0c6735
Ellysk <ekoka56@gmail.com>
2020-12-16 19:38:56 +0000

    Add CravyTabBarController.

---
+++ Cravy-Partner/Controllers/CravyTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  CravyTabBarController.swift
+//  Cravy-Partner
+//
+//  Created by Cravy on 16/12/2020.
+//  Copyright © 2020 Cravy. All rights reserved.
+//
+
+import UIKit
+
+class CravyTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
--- Cravy-Partner/Controllers/ViewController.swift
+++ Cravy-Partner/Controllers/ViewController.swift
@@ -8,9 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum SPLASH {
+    case intro
+    case auth
+}
+
+class SplashController: UIViewController {
+    @IBOutlet weak var pageController: UIView!
+    @IBOutlet weak var getStartedButton: RoundBorderedButton!
+    var authStackView = AuthStackView()
+    @IBOutlet weak var noticeLabel: UILabel!
+    var splash: SPLASH {
+        set {
+            pageController.isHidden = newValue == .auth
+            getStartedButton.isHidden = newValue == .auth
+            authStackView.isHidden = newValue == .intro
+            noticeLabel.isHidden = newValue == .intro
+        }
+        
+        get {
+            if pageController.isHidden && getStartedButton.isHidden {
+                return .auth
+            } else {
+                return .intro
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        splash = .intro
+        setAuthStackView()
+    }
+    
+    private func setAuthStackView() {
+        self.view.addSubview(authStackView)
+        authStackView.translatesAutoresizingMaskIntoConstraints = false
+        authStackView.centerYAnchor(to: self.view)
+        authStackView.HConstraint(to: self.view, constant: 8)
+    }
+    
+    @IBAction func getStarted(_ sender: UIButton) {
+        splash = .auth
     }
 }
