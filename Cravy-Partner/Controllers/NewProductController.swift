78ba41e6ec89460c97ff3f414371af336c1f62a8
Ellysk <ekoka56@gmail.com>
2020-12-23 15:02:59 +0000

    Create NewProductController file. [No functionality]

---
+++ Cravy-Partner/Controllers/NewProductController.swift
@@ -0,0 +1,17 @@
+//
+//  NewProductController.swift
+//  Cravy-Partner
+//
+//  Created by Cravy on 23/12/2020.
+//  Copyright © 2020 Cravy. All rights reserved.
+//
+
+import UIKit
+
+class NewProductController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
